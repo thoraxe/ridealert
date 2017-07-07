7f9d63729f24e42904ac9405d0a771c0b3f6ce57
Erik M Jacobs <erikmjacobs@gmail.com>
2017-07-07 16:05:12 -0400

    Initial skeleton complete

--- app.rb
+++ app.rb
@@ -5,3 +5,27 @@ require 'sass'
 get '/' do
   haml :index
 end
+
+get '/check_in' do
+  haml :check_in
+end
+
+get '/checked_in' do
+  haml :checked_in
+end
+
+get '/check_out' do
+  haml :check_out
+end
+
+get '/warning' do
+  haml :warning
+end
+
+get '/notify_confirm' do
+  haml :notify_confirm
+end
+
+get '/notify' do
+  haml :notify
+end
